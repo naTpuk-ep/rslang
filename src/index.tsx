b5af04e2353e1b59a6ed20f5ce845d89578caa7c
RamanNovikau <demoikk@gmail.com>
2021-03-30 16:05:46 +0300

    refactor: code formate

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
+import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
 import store from "./App/store";
 
 ReactDOM.render(
