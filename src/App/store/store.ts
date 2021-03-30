d061d0dcdd5e1f4c7ec4e409bd4a82149c3a06b5
RamanNovikau <demoikk@gmail.com>
2021-03-30 16:10:58 +0300

    feat: add categories reducer to store

--- src/App/store/store.ts
+++ src/App/store/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import categoriesReducer from "./reducers/categoriesReducer";
 import { wordsReducer } from "./reducers/wordsReducer";
 
 const rootReducer = combineReducers({
-  repos: wordsReducer,
+  words: wordsReducer,
+  categories: categoriesReducer,
 });
 
 const store = createStore(
