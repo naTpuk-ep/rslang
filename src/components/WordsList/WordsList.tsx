cf28016b4b0e299115d88c128d750b7e2045a2d7
RamanNovikau <demoikk@gmail.com>
2021-03-31 09:19:29 +0300

    refactor: change words reducer

--- src/components/WordsList/WordsList.tsx
+++ src/components/WordsList/WordsList.tsx
@@ -1,32 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { nanoid } from "nanoid";
 import React, { useEffect } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
-import useHttp from "../../../hooks/http.hook";
-import IWordData from "../../../models/word-model";
-import { setWords } from "../../store/reducers/wordsReducer";
+import useActions from "../../hooks/useActions";
+import useTypedSelector from "../../hooks/useTypeSelector";
 
 interface IWordsListParams {
   groupId: number;
-  fetchWords: (data: IWordData[]) => void;
 }
 
 const WordsList: React.FunctionComponent<IWordsListParams> = (
   props: IWordsListParams
 ) => {
-  const { groupId, fetchWords } = props;
+  const { groupId } = props;
 
-  const { request } = useHttp();
-
-  const words = useSelector(
-    (state: { words: { words: IWordData[] } }) => state.words.words
-  );
+  const { words, error, isFetching } = useTypedSelector((state) => state.words);
+  const { fetchWords } = useActions();
 
   useEffect(() => {
-    request(
-      `https://rnovikov-rs-lang-back.herokuapp.com/words?group=${1}&page=${1}`,
-      "GET"
-    ).then((data) => fetchWords(data));
+    fetchWords();
   }, []);
 
   return (
@@ -38,12 +29,4 @@ const WordsList: React.FunctionComponent<IWordsListParams> = (
   );
 };
 
-const mapDispatchToProps = (
-  dispatch: (arg0: { type: string; payload: IWordData[] }) => void
-) => {
-  return {
-    fetchWords: (data: IWordData[]) => dispatch(setWords(data)),
-  };
-};
-
-export default connect(null, mapDispatchToProps)(WordsList);
+export default WordsList;
