242f4b502e0430a0e47a509fcb97ec7122aefbc8
RamanNovikau <demoikk@gmail.com>
2021-04-02 14:42:21 +0300

    fix: change word list component

--- src/components/WordsList/WordsList.tsx
+++ src/components/WordsList/WordsList.tsx
@@ -1,13 +1,25 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { ButtonGroup, Button, Typography, IconButton } from "@material-ui/core";
 import { nanoid } from "nanoid";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "@material-ui/lab/Pagination";
+import PaginationItem from "@material-ui/lab/PaginationItem";
+import { createStyles, Grid, makeStyles, Theme } from "@material-ui/core";
 import useActions from "../../hooks/useActions";
 import useTypedSelector from "../../hooks/useTypeSelector";
 import WordCard from "../WordCard";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      display: "flex",
+      flexDirection: "column",
+      rowGap: 15,
+      padding: theme.spacing(2, 2),
+    },
+  })
+);
 interface IWordsListProps {
   groupId: number;
 }
@@ -15,48 +27,77 @@ interface IWordsListProps {
 const WordsList: React.FunctionComponent<IWordsListProps> = (
   props: IWordsListProps
 ) => {
+  const classes = useStyles();
   const { groupId } = props;
-  const { words, error, isFetching, page } = useTypedSelector(
-    (state) => state.userWords
-  );
-  const { agregateUserWords, setUserWordsPage } = useActions();
+  const {
+    aggregatedWords,
+    pages,
+    error,
+    isFetching,
+    isPagesFetching,
+    page: userPage,
+  } = useTypedSelector((state) => state.userWords);
+  const { agregateUserWords, setUserWordsPage, fetchPages } = useActions();
 
   useEffect(() => {
-    const filter = { $or: [{ "userWord.status": "hard" }, { userWord: null }] };
+    fetchPages(groupId);
+  }, [groupId]);
 
-    agregateUserWords(groupId, page, 20, JSON.stringify(filter));
-  }, [page]);
+  useEffect(() => {
+    if (pages.length) {
+      const filter = {
+        $or: [{ "userWord.status": { $ne: "deleted" } }, { userWord: null }],
+      };
+      if (!pages[userPage]) {
+        setUserWordsPage(0);
+        return;
+      }
+      agregateUserWords(
+        groupId,
+        pages[userPage]._id,
+        JSON.stringify(filter),
+        1
+      );
+    }
+  }, [pages.length, userPage]);
 
-  const onclickHandler = (number: number) => {
-    setUserWordsPage(number);
+  const onPageChangeHandler = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    if (page !== 0 && page !== 31) {
+      setUserWordsPage(page - 1);
+    }
   };
 
   return (
     <>
-      <div>
-        <IconButton
-          aria-label="prev"
-          disabled={page === 0}
-          onClick={() => {
-            onclickHandler(page - 1);
-          }}
-        >
-          <ArrowBackIcon fontSize="inherit" />
-        </IconButton>
-        <Typography>{page + 1}</Typography>
-        <IconButton
-          aria-label="next"
-          disabled={page === 29}
-          onClick={() => {
-            onclickHandler(page + 1);
-          }}
-        >
-          <ArrowForwardIcon fontSize="inherit" />
-        </IconButton>
-      </div>
-      {words.map((word) => {
-        return <WordCard key={nanoid()} word={word} />;
-      })}
+      {isPagesFetching ? (
+        ""
+      ) : (
+        <>
+          <Pagination
+            page={userPage + 1}
+            count={pages.length}
+            onChange={onPageChangeHandler}
+            renderItem={(item) => (
+              <PaginationItem
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...item}
+              />
+            )}
+          />
+          {isFetching ? (
+            ""
+          ) : (
+            <div className={classes.root}>
+              {aggregatedWords.words.map((word) => {
+                return <WordCard key={nanoid()} word={word} />;
+              })}
+            </div>
+          )}
+        </>
+      )}
     </>
   );
 };
