f5915353fff4a38342d82b917a954773ebe7bcdc
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2021-04-03 17:30:07 +0300

    feat: implement Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,23 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import Drawer from "@material-ui/core/Drawer";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Collapse from "@material-ui/core/Collapse";
 import {
-  MAIN,
+  ExpandLess,
+  ExpandMore,
+  GamesTwoTone,
+  VideogameAssetTwoTone,
+  CollectionsBookmarkTwoTone,
+  LibraryBooksTwoTone,
+  BookmarkTwoTone,
+  EqualizerTwoTone,
+} from "@material-ui/icons";
+import {
   BOOK,
   STUDIED_WORDS,
   DIFFICULT_WORDS,
@@ -13,55 +29,199 @@ import {
   STATISTICS,
 } from "../../constants/routes";
 
-const Menu: React.FunctionComponent = () => {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    list: {
+      width: 280,
+    },
+    fullList: {
+      width: "auto",
+    },
+    nested: {
+      paddingLeft: theme.spacing(4),
+    },
+  })
+);
+
+interface IMenuProps {
+  open: boolean;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  closeMenu: Function;
+}
+
+const Menu: React.FunctionComponent<IMenuProps> = ({
+  open,
+  closeMenu,
+}: IMenuProps) => {
+  const isAuthorization = true;
+  const classes = useStyles();
+  const [openBook, setOpenBook] = useState(false);
+  const [openGame, setOpenGame] = useState(false);
+  const [openDictionary, setOpenDictionary] = useState(false);
+
+  const handleClickBook = () => setOpenBook(!openBook);
+  const handleClickGame = () => setOpenGame(!openGame);
+  const handleClickDictionary = () => setOpenDictionary(!openDictionary);
+
   return (
-    <ul>
-      <li>
-        <Link to={MAIN}>Main</Link>
-      </li>
-      <li>
-        <Link to={`${BOOK}/0/0`}>Book Group 0</Link>
-      </li>
-      <li>
-        <Link to={`${BOOK}/1/0`}>Book Group 1</Link>
-      </li>
-      <li>
-        <Link to={`${BOOK}/2/0`}>Book Group 2</Link>
-      </li>
-      <li>
-        <Link to={`${BOOK}/3/0`}>Book Group 3</Link>
-      </li>
-      <li>
-        <Link to={`${BOOK}/4/0`}>Book Group 4</Link>
-      </li>
-      <li>
-        <Link to={`${BOOK}/5/0`}>Book Group 5</Link>
-      </li>
-      <li>
-        <Link to={STUDIED_WORDS}>Studied Words</Link>
-      </li>
-      <li>
-        <Link to={DIFFICULT_WORDS}>Difficult Words</Link>
-      </li>
-      <li>
-        <Link to={DELETED_WORDS}>Deleted Words</Link>
-      </li>
-      <li>
-        <Link to={SAVANNAH}>Savannah</Link>
-      </li>
-      <li>
-        <Link to={AUDIO_CALL}>Audio Call</Link>
-      </li>
-      <li>
-        <Link to={SPRINT}>Sprint</Link>
-      </li>
-      <li>
-        <Link to={OWN_GAME}>Own Game</Link>
-      </li>
-      <li>
-        <Link to={STATISTICS}>Statistics</Link>
-      </li>
-    </ul>
+    <Drawer anchor="left" open={open} onClose={closeMenu(false)}>
+      <div className={classes.list} role="presentation">
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem button onClick={handleClickBook}>
+            <ListItemIcon>
+              <CollectionsBookmarkTwoTone />
+            </ListItemIcon>
+            <ListItemText primary="Учебник" />
+            {openBook ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={openBook} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding onClick={closeMenu(false)}>
+              <LinkContainer to={`${BOOK}/0/0`}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <BookmarkTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Раздел 1" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={`${BOOK}/1/0`}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <BookmarkTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Раздел 2" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={`${BOOK}/2/0`}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <BookmarkTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Раздел 3" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={`${BOOK}/3/0`}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <BookmarkTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Раздел 4" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={`${BOOK}/4/0`}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <BookmarkTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Раздел 5" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={`${BOOK}/5/0`}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <BookmarkTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Раздел 6" />
+                </ListItem>
+              </LinkContainer>
+            </List>
+          </Collapse>
+
+          <ListItem button onClick={handleClickGame}>
+            <ListItemIcon>
+              <VideogameAssetTwoTone />
+            </ListItemIcon>
+            <ListItemText primary="Игры" />
+            {openGame ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={openGame} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding onClick={closeMenu(false)}>
+              <LinkContainer to={SAVANNAH}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <GamesTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Саванна" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={AUDIO_CALL}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <GamesTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Аудио вызов" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={SPRINT}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <GamesTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Спринт" />
+                </ListItem>
+              </LinkContainer>
+              <LinkContainer to={OWN_GAME}>
+                <ListItem button className={classes.nested}>
+                  <ListItemIcon>
+                    <GamesTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Своя игра" />
+                </ListItem>
+              </LinkContainer>
+            </List>
+          </Collapse>
+
+          {isAuthorization && (
+            <>
+              <ListItem button onClick={handleClickDictionary}>
+                <ListItemIcon>
+                  <LibraryBooksTwoTone />
+                </ListItemIcon>
+                <ListItemText primary="Словарь" />
+                {openDictionary ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={openDictionary} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding onClick={closeMenu(false)}>
+                  <LinkContainer to={STUDIED_WORDS}>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <BookmarkTwoTone />
+                      </ListItemIcon>
+                      <ListItemText primary="Изучаемые слова" />
+                    </ListItem>
+                  </LinkContainer>
+                  <LinkContainer to={DIFFICULT_WORDS}>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <BookmarkTwoTone />
+                      </ListItemIcon>
+                      <ListItemText primary="Сложные слова" />
+                    </ListItem>
+                  </LinkContainer>
+                  <LinkContainer to={DELETED_WORDS}>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <BookmarkTwoTone />
+                      </ListItemIcon>
+                      <ListItemText primary="Удаленные слова" />
+                    </ListItem>
+                  </LinkContainer>
+                </List>
+              </Collapse>
+
+              <LinkContainer to={STATISTICS}>
+                <ListItem button onClick={closeMenu(false)}>
+                  <ListItemIcon>
+                    <EqualizerTwoTone />
+                  </ListItemIcon>
+                  <ListItemText primary="Статистика" />
+                </ListItem>
+              </LinkContainer>
+            </>
+          )}
+        </List>
+      </div>
+    </Drawer>
   );
 };
 
