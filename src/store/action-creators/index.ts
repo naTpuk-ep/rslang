bebb71aa4dbb13e470052fe186a80b14fbc64fa9
Raman Novikau <demoikk@gmail.com>
2021-04-05 01:14:04 +0300

    feat: add statistics actions and reducer imports

--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -1,7 +1,9 @@
 import * as WordsActionsCreators from "./words";
 import * as UserWordsActionsCreators from "./userWords";
+import * as StatisticsActionsCreators from "./statistics";
 
 export default {
   ...WordsActionsCreators,
   ...UserWordsActionsCreators,
+  ...StatisticsActionsCreators,
 };
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import categoriesReducer from "./categoriesReducer";
+import statisticsReducer from "./statisticsReducer";
 import userWordsReducer from "./userWordsReducer";
 import wordsReducer from "./wordsReducer";
 
@@ -7,6 +8,7 @@ export const rootReducer = combineReducers({
   words: wordsReducer,
   userWords: userWordsReducer,
   categories: categoriesReducer,
+  statistics: statisticsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
