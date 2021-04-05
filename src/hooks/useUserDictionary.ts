4f6892d6c9b0e448e5980bbd7467ad861ec4e16b
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-04-05 11:17:04 +0300

    feat: add useUserDictionary hook

---
+++ src/hooks/useUserDictionary.ts
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import IUserWordData from "../types/userWords-types";
+import useActions from "./useActions";
+import useTypedSelector from "./useTypeSelector";
+
+interface IUseUserDictionaryProps {
+  group: number;
+  page: number;
+  filter: string;
+}
+
+const useUserDictionary = (
+  props: IUseUserDictionaryProps
+): { words: IUserWordData[]; pagesCount: number; isFetching: boolean } => {
+  const { group, page, filter } = props;
+  const { aggregatedWords, isFetching } = useTypedSelector(
+    (state) => state.userWords
+  );
+
+  const { aggregateUserWords } = useActions();
+
+  useEffect(() => {
+    aggregateUserWords(Number(group), Number(page), filter);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [group, page]);
+
+  return {
+    words: aggregatedWords.words,
+    pagesCount: Math.ceil(aggregatedWords.totalCount / 20),
+    isFetching,
+  };
+};
+
+export default useUserDictionary;
--- src/pages/Dictionary/Difficult/DifficultPage.tsx
+++ src/pages/Dictionary/Difficult/DifficultPage.tsx
@@ -1,12 +1,9 @@
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import useActions from "../../../hooks/useActions";
-import useTypedSelector from "../../../hooks/useTypeSelector";
 import { GET_USER_HARD_WORDS_FILTER } from "../../../constants/requestParams";
 import WordsList from "../../../components/WordsList";
 import { DIFFICULT_WORDS } from "../../../constants/routes";
+import useUserDictionary from "../../../hooks/useUserDictionary";
 
 export interface IDifficultPageParams {
   group: string;
@@ -15,42 +12,25 @@ export interface IDifficultPageParams {
 
 const DifficultPage: React.FunctionComponent = () => {
   const { group, page } = useParams<IDifficultPageParams>();
-
-  // useUserDifficult
-
-  const { aggregatedWords, isFetching } = useTypedSelector(
-    (state) => state.userWords
-  );
-  // const history = useHistory();
-  const { aggregateUserWords } = useActions();
-
-  useEffect(() => {
-    aggregateUserWords(
-      Number(group),
-      Number(page),
-      JSON.stringify(GET_USER_HARD_WORDS_FILTER)
-    );
-  }, [group, page]);
-
-  // useUserDifficult
-
-  const props = {
-    route: DIFFICULT_WORDS,
+  const { words, pagesCount, isFetching } = useUserDictionary({
     group: Number(group),
     page: Number(page),
-    words: aggregatedWords.words,
-    pagesCount: Math.ceil(aggregatedWords.totalCount / 20),
-    isFetching,
-    isPagesFetching: false,
-    difficultCategory: true,
-  };
+    filter: JSON.stringify(GET_USER_HARD_WORDS_FILTER),
+  });
 
   return (
     <>
-      <h1>
-        Difficult Group: {group} Page: {page}
-        <WordsList {...props} />
-      </h1>
+      Difficult Group: {group} Page: {page}
+      <WordsList
+        route={DIFFICULT_WORDS}
+        group={Number(group)}
+        page={Number(page)}
+        words={words}
+        pagesCount={pagesCount}
+        isFetching={isFetching}
+        isPagesFetching={false}
+        difficultCategory
+      />
     </>
   );
 };
