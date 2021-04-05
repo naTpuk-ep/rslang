b50173f59df9eeaee74a6d96aab58c61309f173c
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-04-05 11:40:04 +0300

    refactor: rename and move files

--- src/pages/Dictionary/DictionaryPage.tsx
+++ src/pages/Dictionary/DictionaryPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
 import { GET_USER_HARD_WORDS_FILTER } from "../../../constants/requestParams";
 import WordsList from "../../../components/WordsList";
 import { DIFFICULT_WORDS } from "../../../constants/routes";
 import useUserDictionary from "../../../hooks/useUserDictionary";
+import WordsCategories from "../../../components/WordsCategories";
 
 export interface IDifficultPageParams {
   group: string;
@@ -20,7 +22,10 @@ const DifficultPage: React.FunctionComponent = () => {
 
   return (
     <>
-      Difficult Group: {group} Page: {page}
+      <Typography variant="h3" gutterBottom>
+        Сложные слова
+      </Typography>
+      <WordsCategories route={DIFFICULT_WORDS} />
       <WordsList
         route={DIFFICULT_WORDS}
         group={Number(group)}
