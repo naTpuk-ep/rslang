6d308bb353fc23f9735a0355880d5ee359b334b3
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-04-07 16:26:34 +0300

    feat: add useBackToMain & connect store & add ILocationState

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,12 +1,35 @@
 import * as React from "react";
+import { useHistory } from "react-router-dom";
 import GameNames from "../../constants/game-names";
+import Locations from "../../constants/locations";
+import { MAIN } from "../../constants/routes";
+import useTypedSelector from "../../hooks/useTypeSelector";
+// import { IUserWordData } from "../../types/user-words-types";
 import "./Game.scss";
 
 interface IGameProps {
   game: GameNames;
 }
 
+interface ILocationState {
+  from: Locations;
+}
+
+const useBackToMain = () => {
+  const history = useHistory<ILocationState>();
+
+  if (!history.location.state?.from) history.push(MAIN);
+};
+
 const Game: React.FunctionComponent<IGameProps> = ({ game }: IGameProps) => {
+  useBackToMain();
+
+  const history = useHistory<ILocationState>();
+  const { aggregatedWords } = useTypedSelector((state) => state.userWords);
+  const { words } = aggregatedWords;
+
+  // eslint-disable-next-line no-console
+  console.log(history.location.state?.from, game, words);
   return <div className="game-page" />;
 };
 
