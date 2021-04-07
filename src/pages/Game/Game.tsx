d984682541a7911ebee85f1b58fd2367eaa3d7a9
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2021-04-07 18:48:34 +0300

    fix: rename and change hook useBackTo

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -14,18 +14,29 @@ interface ILocationState {
   from: Locations;
 }
 
-const useBackToMain = () => {
+const useBackTo = () => {
   const history = useHistory<ILocationState>();
 
-  if (!history.location.state?.from) history.push(MAIN);
+  const backToPreviousPage = () => history.goBack();
+  const backToPage = (route: string) => history.push(route);
+  const backToMain = () => {
+    if (!history.location.state?.from) history.push(MAIN);
+  };
+
+  return {
+    backToPreviousPage,
+    backToPage,
+    backToMain,
+  };
 };
 
 const Game: React.FunctionComponent<IGameProps> = ({ game }: IGameProps) => {
-  useBackToMain();
-
+  const { backToPreviousPage, backToPage, backToMain } = useBackTo();
   const history = useHistory<ILocationState>();
   const { aggregatedWords } = useTypedSelector((state) => state.userWords);
   const { words } = aggregatedWords;
+
+  backToMain();
 
   // eslint-disable-next-line no-console
   console.log(history.location.state?.from, game, words);
