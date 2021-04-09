9a36b10d89daa6165d49d82cb68faacef70c9db8
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-04-09 15:03:12 +0300

    feat: change StartDialog & add styles for StartDialog

--- src/components/StartDialog/StartDialog.tsx
+++ src/components/StartDialog/StartDialog.tsx
@@ -1,47 +1,72 @@
-import React from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import React, { useState } from "react";
 import Dialog from "@material-ui/core/Dialog";
+import Button from "@material-ui/core/Button";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { makeStyles, createStyles, Theme } from "@material-ui/core";
+import { BookmarkTwoTone } from "@material-ui/icons";
+import "./StartDialog.scss";
 
 const groups = [
-  "Раздел 1",
-  "Раздел 2",
-  "Раздел 3",
-  "Раздел 4",
-  "Раздел 5",
-  "Раздел 6",
+  { text: "Раздел 1", color: "#f6d258" },
+  { text: "Раздел 2", color: "#efcec5" },
+  { text: "Раздел 3", color: "#d1af94" },
+  { text: "Раздел 4", color: "#97d5e0" },
+  { text: "Раздел 5", color: "#88b14b" },
+  { text: "Раздел 6", color: "#ef562d" },
 ];
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      "& > * + *": {
+        marginTop: theme.spacing(2),
+      },
+    },
+  })
+);
 
 export interface IStartDialogProps {
   open: boolean;
+  group: number | null;
   selectGroup: (value: number) => void;
 }
 
 const StartDialog: React.FunctionComponent<IStartDialogProps> = (
   props: IStartDialogProps
 ) => {
-  const { selectGroup, open } = props;
-
+  const classes = useStyles();
+  const { selectGroup, open, group } = props;
+  const [isSelectedGroup, setIsSelectedGroup] = useState<boolean>(false);
   const handleListItemClick = (value: number) => {
-    selectGroup(value);
+    if (!isSelectedGroup) selectGroup(value);
+    setIsSelectedGroup(true);
   };
 
   return (
     <Dialog aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle id="simple-dialog-title">Выберите раздел</DialogTitle>
-      <List>
-        {groups.map((group, index) => (
-          <ListItem
-            button
+      <div className="dialog">
+        {groups.map((item, index) => (
+          <Button
             onClick={() => handleListItemClick(index)}
-            key={group}
+            key={item.text}
+            style={{ backgroundColor: item.color }}
+            variant="contained"
+            size="large"
+            startIcon={
+              isSelectedGroup && group === index ? "" : <BookmarkTwoTone />
+            }
           >
-            <ListItemText primary={group} />
-          </ListItem>
+            {isSelectedGroup && group === index ? (
+              <div className={classes.root}>
+                <LinearProgress />
+              </div>
+            ) : (
+              item.text
+            )}
+          </Button>
         ))}
-      </List>
+      </div>
     </Dialog>
   );
 };
