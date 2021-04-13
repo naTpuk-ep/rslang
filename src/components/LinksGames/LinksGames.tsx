ad78e5a3d9deb5f9028a80dfe36a143729bd472c
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-04-13 14:04:17 +0300

    feat: add LinksGames

---
+++ src/components/LinksGames/LinksGames.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Button } from "@material-ui/core";
+import { GamesTwoTone } from "@material-ui/icons";
+import { nanoid } from "nanoid";
+import Locations from "../../constants/locations";
+import { OWN_GAME, SAVANNAH, AUDIO_CALL, SPRINT } from "../../constants/routes";
+import "./LinksGames.scss";
+
+const linksData = [
+  { name: "Саванна", route: SAVANNAH },
+  { name: "Аудио вызов", route: AUDIO_CALL },
+  { name: "Спринт", route: SPRINT },
+  { name: "Своя игра", route: OWN_GAME },
+];
+
+interface ILinksGamesProps {
+  group: number;
+  page: number;
+  filter: string;
+  wordsPerPage: number;
+}
+
+const LinksGames: React.FunctionComponent<ILinksGamesProps> = ({
+  group,
+  page,
+  filter,
+  wordsPerPage,
+}: ILinksGamesProps) => {
+  return (
+    <div className="links-game">
+      {linksData.map((item) => {
+        return (
+          <LinkContainer
+            key={nanoid()}
+            to={{
+              pathname: item.route,
+              state: {
+                from: Locations.Book,
+                group,
+                page,
+                filter,
+                wordsPerPage,
+              },
+            }}
+          >
+            <Button
+              variant="contained"
+              color="default"
+              startIcon={<GamesTwoTone />}
+            >
+              {item.name}
+            </Button>
+          </LinkContainer>
+        );
+      })}
+    </div>
+  );
+};
+
+export default LinksGames;
