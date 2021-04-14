88cbca53929e5ebaf5cc3dc28e6a3e1dcdc181eb
RamanNovikau <demoikk@gmail.com>
2021-04-14 15:39:11 +0300

    feat: add fetch stat on page

--- src/pages/GameSavannah/GameSavannah.tsx
+++ src/pages/GameSavannah/GameSavannah.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import Countdown from "react-countdown";
